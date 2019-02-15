c26a187a7054336d907ac5a13b4f6e9b5b7971d7
g-harel <gabrielj.harel@gmail.com>
2019-02-15 00:16:13 -0500

    update cloudbuild trigger for google provider v2

--- terraform/google_cloudbuild.tf
+++ terraform/google_cloudbuild.tf
@@ -1,6 +1,6 @@
 resource "google_cloudbuild_trigger" "master_branch" {
     trigger_template = {
-        project = "${data.google_project.project.project_id}"
+        project_id = "${data.google_project.project.project_id}"
         repo_name = "${google_sourcerepo_repository.website.name}"
         branch_name = "master"
         dir = "terraform"
@@ -9,11 +9,11 @@ resource "google_cloudbuild_trigger" "master_branch" {
     build = {
         step = {
             name = "${data.google_container_registry_image.terraform_build_step.image_url}"
-            args = "init"
+            args = ["init"]
         }
         step = {
             name = "${data.google_container_registry_image.terraform_build_step.image_url}"
-            args = "apply -auto-approve"
+            args = ["apply", "-auto-approve"]
         }
     }
 }
